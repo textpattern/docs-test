3e96255bc5a061e243b3e7f935f44941e81f21e5
Pete Cooper <pete@apropos.local>
2025-04-14 20:10:16 +0100

    ffs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'textpattern-docs-test.readthedocs.io',
+  url: 'https://textpattern-docs-test.readthedocs.io',
   baseUrl: '/en/latest/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
